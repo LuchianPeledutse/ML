167afc07e4b705a47a8d3bbd3a933f7134505027
luchian <luchian-1999@mail.ru>
2025-09-20 18:49:44 +0300

    Changes: 1. Added correct masking for target values 2. Correct writing operations to leaf nodes

--- ML_models.py
+++ ML_models.py
@@ -12,7 +12,22 @@ class TreeNode(object):
         self.right = None
     
     def __str__(self):
-        return ' '.join([str(item) for item in self.data.items()])
+        return_string = ''
+        double_space = '\n\n'
+        #writing parameters
+        t ='t: ' + str(self.data['t']) + '\n'
+        class_ = 'class: ' + str(self.data['class']) + '\n'
+        total_samples = 'total samples: ' + str(self.data['total_samples']) + '\n'
+        class_samples = 'class samples: ' + str(self.data['each_class_samples']) + '\n'
+        gini = 'Gini: ' + str(self.data['data_gini_coef']) + '\n'
+        return_string += t
+        return_string += class_
+        return_string += total_samples
+        return_string += class_samples
+        return_string += gini
+        return return_string
+
+
 
 
 
@@ -29,6 +44,7 @@ class DecisionTreeClassifier:
         self.best_class = None
         self.best_t = None
         self.data = dict()
+        self.best_loss = float('inf')
         #The BinaryTree
         self.BinaryTree = TreeNode()
 
@@ -43,8 +59,6 @@ class DecisionTreeClassifier:
         flag_added = False
         number_of_classes = X_data.shape[1]
         number_of_instances = X_data.shape[0]
-        best_row = None
-        best_loss = float('inf')
         #Training algorithm
         if number_of_instances == 1:
             return self
@@ -52,25 +66,27 @@ class DecisionTreeClassifier:
             #main learning loop
             for feature_col in range(number_of_classes):
                 for instance_row in range(number_of_instances):
+                    #getting the t devider and masking for <= and > 
                     t_col = X_data[instance_row,feature_col]
-                    y_left = y_data[:instance_row,:]
-                    y_right = y_data[instance_row:,:]
+                    t_mask = X_data[X_data[:,feature_col]] <= t_col
+                    not_t_mask = X_data[X_data[:,feature_col]] > t_col
+                    y_left = y_data[t_mask,:]
+                    y_right = y_data[not_t_mask,:]
                     #getting the gini losses for both sides
-                    total_samples_left, each_cls_sample_left, gini_coef_left = self.calc_gini(y_left)
-                    total_samples_right, each_cls_sample_right, gini_coef_right = self.calc_gini(y_right)
+                    total_samples_left, _, gini_coef_left = self.calc_gini(y_left)
+                    total_samples_right, _, gini_coef_right = self.calc_gini(y_right)
                     total_instances = total_samples_right + total_samples_left
                     J_loss = total_samples_left/total_instances*gini_coef_left + total_samples_right/total_instances*gini_coef_right
-                    if J_loss < best_loss:
+                    if J_loss < self.best_loss:
                         #checking the necessary parameters
-                        best_loss = J_loss
-                        best_row = instance_row
+                        self.best_loss = J_loss
                         self.best_class = feature_col
                         self.best_t = t_col
                         flag_added = True
             #adding the necessary information to the tree
             data_total_samples, data_each_class_sample, data_gini_coef = self.calc_gini(y_data)
-            self.data['t'] = self.best_t
-            self.data['cls'] = self.best_class
+            self.data['t'] = self.best_t if flag_added else None
+            self.data['class'] = self.best_class if flag_added else None
             self.data['total_samples'] = data_total_samples
             self.data['each_class_samples'] = data_each_class_sample
             self.data['data_gini_coef'] = data_gini_coef
@@ -83,8 +99,8 @@ class DecisionTreeClassifier:
                 node_right = TreeNode()
                 main_node.left = node_left
                 main_node.right = node_right
-                self.fit(X_data[:best_row,:], y_data[:best_row,:], node = node_left)
-                self.fit(X_data[best_row+1:,:], y_data[best_row+1:,:], node = node_right)
+                self.fit(X_data[X_data[:,self.best_class] <= self.best_t,:], y_data[X_data[:,self.best_class] <= self.best_t,:], node = node_left)
+                self.fit(X_data[X_data[:,self.best_class] > self.best_t,:], y_data[X_data[:,self.best_class] > self.best_t,:], node = node_right)
 
     def calc_gini(self, y_vector:ndarray):
         """
