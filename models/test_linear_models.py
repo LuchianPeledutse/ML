043927a84c6b52ca289b2a73f88f3e45a396dc1a
Luchian <luchianpeledutse@gmail.com>
2025-12-22 22:32:10 +0300

    Logistic regression tested on accuracy similarity with sklearn implementation. A lot of warnings for loss function popped up, this needs fixing

--- models/test_linear_models.py
+++ models/test_linear_models.py
@@ -6,10 +6,14 @@ import pytest
 
 import numpy as np
 from sklearn import linear_model
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import make_classification
 
-from linear_models import LinearRegression
+from linear_models import LinearRegression, LogisticRegression
 
-NUM_TESTS = 1_000
+NUM_TESTS = 10
+NUM_SAMPLES = 1_000
+NUM_FEATURES = 2
 
 X_ = [np.random.uniform(-10, 10, 1000) for _ in range(NUM_TESTS)]
 y_ = [np.random.uniform(-10, 10, 1)*x_arr+np.random.uniform(-10, 10, 1) for x_arr in X_]
@@ -22,7 +26,7 @@ def test_dataset_linear_learning(X: np.ndarray, y: np.ndarray, eps=1e-2) -> None
     Args
     ----
     X: np.ndarray
-        Feature matrix X of shape NxD where N is number of objects, D is number of features
+        Feature matrix X of shape Nx1 where N is number of objects, 1 is number of features
     
     y: np.ndarray
         Target vector column
@@ -41,7 +45,17 @@ def test_dataset_linear_learning(X: np.ndarray, y: np.ndarray, eps=1e-2) -> None
     assert (diff < eps).any(), "Some of coefficients difference is larger than epsilon"
 
 
-def test_dataset_linear_logistic_learning() -> None:
+log_reg_testing_set = [make_classification(
+    n_samples=1000,          
+    n_features=2,           
+    n_informative=2,         
+    n_redundant=0,           
+    n_clusters_per_class=1 
+) for _ in range(NUM_TESTS)]
+
+
+@pytest.mark.parametrize("X,y", log_reg_testing_set)
+def test_dataset_linear_logistic_learning(X: np.ndarray, y: np.ndarray, eps: float = 7e-2) -> None:
     """
     Tests whether implemented model solution is similar to sklearn solution
 
@@ -52,5 +66,19 @@ def test_dataset_linear_logistic_learning() -> None:
     
     y: np.ndarray
         Target vector column (binary values 0 and 1)
+    
+    eps: float
+        Epsilon up to which the accuracies have to be the same
     """
-    assert 0 == 0
+    # Preparing models
+    my_logreg = LogisticRegression(epoch=100_000, alpha=7e-1)
+    sk_logreg = linear_model.LogisticRegression()
+    # Fitting them
+    my_logreg.fit(X, y.reshape(-1, 1))
+    sk_logreg.fit(X, y)
+    # Comparing accuracies
+    my_acc = accuracy_score(y_pred=my_logreg.predict(X).reshape(-1), y_true=y)
+    sk_acc = accuracy_score(y_pred=sk_logreg.predict(X), y_true=y)
+    assert abs(my_acc-sk_acc) < eps, "The differences between accuracies is larger than epsilon"
+
+
